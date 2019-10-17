e5f7340d85737e58595f481f24fe228f78919654
Matthew Pitcher <mpitcher@starrez.com>
2019-10-17 02:20:54 -0600

    fix id mapping by using the ORM

--- create_collections.py
+++ create_collections.py
@@ -1,102 +1,122 @@
 import os
 from Helpers.CollectionFunctions import CollectionFunctions
-import pymongo
 import datetime
 import random
+from mongoengine import connect
+from Models.Project import Project as ProjectModel
+from Models.Sprint import Sprint as SprintModel
+from Models.SprintProject import SprintProject as SprintProjectModel
+from Models.User import User as UserModel
+from Models.UserTeam import UserTeam as UserTeamModel
+from Models.Team import Team as TeamModel
+from Models.Ticket import Ticket as TicketModel
 from bson import ObjectId
 from hashlib import sha224
-from GLOBAL import (CLIENT_ENV_KEY, DB_NAME, PROJECT_COLLECTION, SPRINT_COLLECTION, SPRINT_PROJECT_COLLECTION,
-                    TEAM_COLLECTION, TICKET_COLLECTION, USER_COLLECTION, USER_TEAM_COLLECTION)
+from GLOBAL import CLIENT_ENV_KEY, DB_NAME
+
 
 def main():
     os.environ[CLIENT_ENV_KEY] = "mongodb://localhost:27017/"
     collectionFunctions = CollectionFunctions()
-    mongo_client = pymongo.MongoClient(os.environ[CLIENT_ENV_KEY])
-    db = mongo_client[DB_NAME]
+    connect(DB_NAME, host=os.environ[CLIENT_ENV_KEY],
+            alias="default")
 
-    users = db[USER_COLLECTION]
     admin_id = ObjectId()
     test_user_id = ObjectId()
-    users.insert_one({"id": admin_id, "email": "admin@test.com",
-                      "password": sha224(b"admin").hexdigest(), "first_name": "Admin", "last_name": "User"})
-    users.insert_one({"id": test_user_id, "email": "test_user@test.com",
-                      "password": sha224(b"password").hexdigest(), "first_name": "Test", "last_name": "User"})
+    user = UserModel(id=admin_id, email="admin@test.com", password=sha224(
+        b"admin").hexdigest(), first_name="Admin", last_name="User")
+    user.save()
+    user = UserModel(id=test_user_id, email="test_user@test.com", password=sha224(
+        b"password").hexdigest(), first_name="Test", last_name="User")
+    user.save()
 
-    teams = db[TEAM_COLLECTION]
     canyon_id = ObjectId()
     ridge_id = ObjectId()
     peak_id = ObjectId()
-    teams.insert_one({"id": canyon_id, "team_name": "Canyon",
-                      "status": "Active", "date_created": datetime.datetime.now()})
-    teams.insert_one({"id": ridge_id, "team_name": "Ridge",
-                      "status": "Pending", "date_created": datetime.datetime.now()})
-    teams.insert_one({"id": peak_id, "team_name": "Peak",
-                      "status": "Terminated", "date_created": datetime.datetime.now()})
+    team = TeamModel(id=canyon_id, team_name="Canyon",
+                     status="Active", date_created=datetime.datetime.now())
+    team.save()
+    team = TeamModel(id=ridge_id, team_name="Ridge",
+                     status="Pending", date_created=datetime.datetime.now())
+    team.save()
+    team = TeamModel(id=peak_id, team_name="Peak",
+                     status="Terminated", date_created=datetime.datetime.now())
+    team.save()
 
-    user_teams = db[USER_TEAM_COLLECTION]
-    user_teams.insert_one(
-        {"id": ObjectId(), "user_id": admin_id, "team_id": canyon_id})
-    user_teams.insert_one(
-        {"id": ObjectId(), "user_id": test_user_id, "team_id": canyon_id})
+    userTeam = UserTeamModel(
+        id=ObjectId(), user_id=admin_id, team_id=canyon_id)
+    userTeam.save()
+    userTeam = UserTeamModel(
+        id=ObjectId(), user_id=test_user_id, team_id=canyon_id)
+    userTeam.save()
 
-    projects = db[PROJECT_COLLECTION]
     red_id = ObjectId()
     blue_id = ObjectId()
     gold_id = ObjectId()
-    projects.insert_one({"id": red_id, "project_name": "RED",
-                         "team_id": canyon_id, "description": "Red project description."})
-    projects.insert_one({"id": blue_id, "project_name": "BLUE",
-                         "team_id": ridge_id, "description": "Blue project description."})
-    projects.insert_one({"id": gold_id, "project_name": "GOLD",
-                         "team_id": peak_id, "description": "Gold project description."})
+    project = ProjectModel(id=red_id, project_name="RED",
+                           team_id=canyon_id, description="Red project description.")
+    project.save()
+    project = ProjectModel(id=red_id, project_name="BLUE",
+                           team_id=ridge_id, description="Blue project description.")
+    project.save()
+    project = ProjectModel(id=red_id, project_name="GOLD",
+                           team_id=peak_id, description="Gold project description.")
+    project.save()
 
-    sprints = db[SPRINT_COLLECTION]
     alpha_id = ObjectId()
     beta_id = ObjectId()
     gamma_id = ObjectId()
     delta_id = ObjectId()
-    sprints.insert_one({"id": alpha_id, "sprint_name": "Alpha", "goal": "Do some work on the project.",
-                        "date_start": datetime.datetime.now()+datetime.timedelta(days=-15),
-                        "date_end": datetime.datetime.now()+datetime.timedelta(days=-1)})
-    sprints.insert_one({"id": beta_id, "sprint_name": "Beta", "goal": "Do MORE work on the project!",
-                        "date_start": datetime.datetime.now()+datetime.timedelta(days=0),
-                        "date_end": datetime.datetime.now()+datetime.timedelta(days=14)})
-    sprints.insert_one({"id": gamma_id, "sprint_name": "Gamma", "goal": "Keep doing work on the project.",
-                        "date_start": datetime.datetime.now()+datetime.timedelta(days=15),
-                        "date_end": datetime.datetime.now()+datetime.timedelta(days=29)})
-    sprints.insert_one({"id": delta_id, "sprint_name": "Delta", "goal": "Complete the project.",
-                        "date_start": datetime.datetime.now()+datetime.timedelta(days=30),
-                        "date_end": datetime.datetime.now()+datetime.timedelta(days=44)})
+    sprint = SprintModel(id=alpha_id, sprint_name="Alpha", goal="Do some work on the project.", date_start=datetime.datetime.now(
+    )+datetime.timedelta(days=-15), date_end=datetime.datetime.now()+datetime.timedelta(days=-1))
+    sprint.save()
+    sprint = SprintModel(id=beta_id, sprint_name="Beta", goal="Do some work on the project!", date_start=datetime.datetime.now(
+    )+datetime.timedelta(days=0), date_end=datetime.datetime.now()+datetime.timedelta(days=14))
+    sprint.save()
+    sprint = SprintModel(id=gamma_id, sprint_name="Gamma", goal="Keep doing work on the project.", date_start=datetime.datetime.now(
+    )+datetime.timedelta(days=15), date_end=datetime.datetime.now()+datetime.timedelta(days=29))
+    sprint.save()
+    sprint = SprintModel(id=delta_id, sprint_name="Delta", goal="Complete the project.", date_start=datetime.datetime.now(
+    )+datetime.timedelta(days=30), date_end=datetime.datetime.now()+datetime.timedelta(days=44))
+    sprint.save()
 
-    sprint_projects = db[SPRINT_PROJECT_COLLECTION]
-    sprint_projects.insert_many([
-        {"id": ObjectId(), "sprint_id": alpha_id,
-         "project_id": red_id},
-        {"id": ObjectId(), "sprint_id": alpha_id,
-         "project_id": blue_id},
-        {"id": ObjectId(), "sprint_id": alpha_id,
-         "project_id": gold_id},
-        {"id": ObjectId(), "sprint_id": beta_id,
-         "project_id": red_id},
-        {"id": ObjectId(), "sprint_id": beta_id,
-         "project_id": blue_id},
-        {"id": ObjectId(), "sprint_id": beta_id,
-         "project_id": gold_id},
-        {"id": ObjectId(), "sprint_id": gamma_id,
-         "project_id": red_id},
-        {"id": ObjectId(), "sprint_id": gamma_id,
-         "project_id": blue_id},
-        {"id": ObjectId(), "sprint_id": gamma_id,
-         "project_id": gold_id},
-        {"id": ObjectId(), "sprint_id": delta_id,
-         "project_id": red_id},
-        {"id": ObjectId(), "sprint_id": delta_id,
-         "project_id": blue_id},
-        {"id": ObjectId(), "sprint_id": delta_id,
-         "project_id": gold_id}
-    ])
+    sprintProject = SprintProjectModel(
+        id=ObjectId(), sprint_id=alpha_id, project_id=red_id)
+    sprintProject.save()
+    sprintProject = SprintProjectModel(
+        id=ObjectId(), sprint_id=alpha_id, project_id=blue_id)
+    sprintProject.save()
+    sprintProject = SprintProjectModel(
+        id=ObjectId(), sprint_id=alpha_id, project_id=gold_id)
+    sprintProject.save()
+    sprintProject = SprintProjectModel(
+        id=ObjectId(), sprint_id=beta_id, project_id=red_id)
+    sprintProject.save()
+    sprintProject = SprintProjectModel(
+        id=ObjectId(), sprint_id=beta_id, project_id=blue_id)
+    sprintProject.save()
+    sprintProject = SprintProjectModel(
+        id=ObjectId(), sprint_id=beta_id, project_id=gold_id)
+    sprintProject.save()
+    sprintProject = SprintProjectModel(
+        id=ObjectId(), sprint_id=gamma_id, project_id=red_id)
+    sprintProject.save()
+    sprintProject = SprintProjectModel(
+        id=ObjectId(), sprint_id=gamma_id, project_id=blue_id)
+    sprintProject.save()
+    sprintProject = SprintProjectModel(
+        id=ObjectId(), sprint_id=gamma_id, project_id=gold_id)
+    sprintProject.save()
+    sprintProject = SprintProjectModel(
+        id=ObjectId(), sprint_id=delta_id, project_id=red_id)
+    sprintProject.save()
+    sprintProject = SprintProjectModel(
+        id=ObjectId(), sprint_id=delta_id, project_id=blue_id)
+    sprintProject.save()
+    sprintProject = SprintProjectModel(
+        id=ObjectId(), sprint_id=delta_id, project_id=gold_id)
+    sprintProject.save()
 
-    tickets = db[TICKET_COLLECTION]
     ticket_descriptions = [
         "Do some work in the app and make things talk to other things.",
         "Research that issue we talked about last week and leave notes on the job.",
@@ -137,16 +157,17 @@ def main():
         project = project_names[random.randint(0, len(project_names)-1)]
         ticket_number = collectionFunctions.findNextId(
             "Ticket", {"project_name": project}, "ticket_number")
-        tickets.insert_one({
-            "id": ObjectId(),
-            "ticket_number": ticket_number,
-            "project_name": project,
-            "sprint_name": sprint_names[random.randint(0, len(sprint_names)-1)],
-            "ticket_type": ticket_types[random.randint(0, len(ticket_types)-1)],
-            "priority": priorities[random.randint(0, len(priorities)-1)],
-            "story_points": story_points[random.randint(0, len(story_points)-1)],
-            "description": description
-        })
+        ticket = TicketModel(id=ObjectId(), ticket_number=ticket_number, project_name=project,
+                             sprint_name=sprint_names[random.randint(
+                                 0, len(sprint_names)-1)],
+                             ticket_type=ticket_types[random.randint(
+                                 0, len(ticket_types)-1)],
+                             priority=priorities[random.randint(
+                                 0, len(priorities)-1)],
+                             story_points=story_points[random.randint(
+                                 0, len(story_points)-1)],
+                             description=description)
+        ticket.save()
 
 
 if __name__ == '__main__':
